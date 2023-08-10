4fe5bc61a0e8b590da9e59e850a327d91de5e360
Anorboyev-Xayrullo <anorboyevxayrllo841@gamil.com>
2023-08-10 22:59:28 +0500

    main

---
+++ main.go
@@ -0,0 +1,45 @@
+package main 
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+)
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles [] Article
+
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+
+	articles := Articles{
+		Article{Title:"Test  Title", Desc:"Test Description",Content:"Hello World"}
+	}
+
+
+
+	fmt.Println("Endpoint Hit: All Articles EndPoint")
+	json.NewEncode(w).Encode(articles)
+}
+
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page EndPoint Hit" )
+}
+
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main () {
+	handleRequests()
+}
